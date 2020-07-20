5223415292006fc8cae4d973aede333905f4d3d5
Rajkumar P <raj@diskodev.com>
2020-07-20 22:02:38 +0530

    Remove gprof dependencies

--- src/profile.cpp
+++ src/profile.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cassert>
 #include <iostream>
 
 #include "sorting_algorithms.hpp"
@@ -24,21 +26,25 @@ int main(int argc, char *argv[])
     std::cout << "Running bubble sort on "<< numbers_to_sort.size()
               << " elements" << std::endl;
     bubble_sort(numbers_to_sort);
+    assert(std::is_sorted(numbers_to_sort.begin(), numbers_to_sort.end()));
 
     numbers_to_sort.assign(numbers.begin(), numbers.end());
     std::cout << "Running selection sort on "<< numbers_to_sort.size()
               << " elements" << std::endl;
     selection_sort(numbers_to_sort);
+    assert(std::is_sorted(numbers_to_sort.begin(), numbers_to_sort.end()));
 
     numbers_to_sort.assign(numbers.begin(), numbers.end());
     std::cout << "Running insertion sort on "<< numbers_to_sort.size()
               << " elements" << std::endl;
     insertion_sort(numbers_to_sort);
+    assert(std::is_sorted(numbers_to_sort.begin(), numbers_to_sort.end()));
 
     numbers_to_sort.assign(numbers.begin(), numbers.end());
     std::cout << "Running merge sort on "<< numbers_to_sort.size()
               << " elements" << std::endl;
     merge_sort(numbers_to_sort);
+    assert(std::is_sorted(numbers_to_sort.begin(), numbers_to_sort.end()));
 
     return 0;
 }
