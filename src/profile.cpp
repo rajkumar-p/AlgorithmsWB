2d1d4321b3c28d086dc25cbf041405a47e087b00
Rajkumar P <raj@diskodev.com>
2020-07-20 19:55:25 +0530

    Add new executable for profiling

---
+++ src/profile.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include "sorting_algorithms.hpp"
+
+std::vector<unsigned int> read_input()
+{
+    unsigned int N;
+    std::cin >> N;
+
+    std::vector<unsigned int> numbers(N);
+    for (unsigned int i = 0; i < N; ++i) {
+        std::cin >> numbers[i];
+    }
+
+    return numbers;
+}
+
+int main(int argc, char *argv[])
+{
+    std::vector<unsigned int> numbers = read_input();
+    std::vector<unsigned int> numbers_to_sort;
+
+    numbers_to_sort.assign(numbers.begin(), numbers.end());
+    std::cout << "Running bubble sort on "<< numbers_to_sort.size()
+              << " elements" << std::endl;
+    bubble_sort(numbers_to_sort);
+
+    numbers_to_sort.assign(numbers.begin(), numbers.end());
+    std::cout << "Running selection sort on "<< numbers_to_sort.size()
+              << " elements" << std::endl;
+    selection_sort(numbers_to_sort);
+
+    numbers_to_sort.assign(numbers.begin(), numbers.end());
+    std::cout << "Running insertion sort on "<< numbers_to_sort.size()
+              << " elements" << std::endl;
+    insertion_sort(numbers_to_sort);
+
+    numbers_to_sort.assign(numbers.begin(), numbers.end());
+    std::cout << "Running merge sort on "<< numbers_to_sort.size()
+              << " elements" << std::endl;
+    merge_sort(numbers_to_sort);
+
+    return 0;
+}
