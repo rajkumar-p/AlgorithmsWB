c1802f5fd9537bccc244a36c4807432599f9ec11
Rajkumar P <raj@diskodev.com>
2021-04-12 16:41:27 +0000

    Add support for matrix transpose

--- python/graph_algorithms.py
+++ python/graph_algorithms.py
@@ -1,8 +1,15 @@
 class Graph:
-    def __init__(self, name):
+    def __init__(self, name, adj_list=None):
         self._name = name
         self._vertices = {}
         self._adj_list = {}
+        if adj_list is not None:
+            for v in adj_list.keys():
+                self._vertices[v.id()] = v
+            self._adj_list = adj_list
+
+    def set_name(self, new_name):
+        self._name = new_name
 
     def add_vertex(self, v_id):
         v = Vertex(v_id)
@@ -15,6 +22,17 @@ class Graph:
         if v1 == None or v2 == None:
             return
         self._adj_list[v1].append(v2)
+
+    def get_trasposed_list(self):
+        new_adj_list = {}
+        for key in self._adj_list.keys():
+            new_adj_list[key] = []
+
+        for key in self._adj_list.keys():
+            for v in self._adj_list[key]:
+                new_adj_list[v].append(key)
+
+        return new_adj_list
 
     def print_graph(self):
         print_header("Graph: {}".format(self._name))
@@ -76,47 +94,20 @@ if __name__ == "__main__":
     g.add_vertex(3)
     g.add_vertex(4)
     g.add_vertex(5)
+    g.add_vertex(6)
 
     g.add_edge(1, 2)
-    g.add_edge(1, 5)
-
-    g.add_edge(2, 1)
-    g.add_edge(2, 3)
-    g.add_edge(2, 4)
+    g.add_edge(1, 4)
     g.add_edge(2, 5)
-
-    g.add_edge(3, 2)
-    g.add_edge(3, 4)
-
+    g.add_edge(3, 5)
+    g.add_edge(3, 6)
     g.add_edge(4, 2)
-    g.add_edge(4, 3)
-    g.add_edge(4, 5)
-
-    g.add_edge(5, 1)
-    g.add_edge(5, 2)
     g.add_edge(5, 4)
+    g.add_edge(6, 6)
 
     g.print_graph()
     g.print_adj_matrix()
 
-    ug = UndirectedGraph("UG 1")
-    ug.add_vertex(1)
-    ug.add_vertex(2)
-    ug.add_vertex(3)
-    ug.add_vertex(4)
-    ug.add_vertex(5)
-
-    ug.add_edge(1, 2)
-    ug.add_edge(1, 5)
-
-    ug.add_edge(2, 3)
-    ug.add_edge(2, 4)
-    ug.add_edge(2, 5)
-
-    ug.add_edge(3, 4)
-
-    ug.add_edge(4, 5)
-
-    ug.print_graph()
-    ug.print_adj_matrix()
-
+    ag = Graph("DG 2", g.get_trasposed_list())
+    ag.print_graph()
+    ag.print_adj_matrix()
